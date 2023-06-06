88793ac2a256493c9a7c0cc1a62185ab9cfb574c
Ikotun Collins <danlogan2003@gmail.com>
2023-06-06 22:43:36 +0100

    props' git push -u origin main

--- src/components/ChatHead.vue
+++ src/components/ChatHead.vue
@@ -2,10 +2,19 @@
     <div class="py-2 px-4 bg-blue-950 h-20 flex">
         <img class="h-16 w-16 rounded-full " :src="require('../assets/images/832.jpg')" >
         <div class="ml-4">
-      <h2 class="text-white font-extrabold">Collins</h2>
-      <h4 class="text-white sm:text-xs lg:text-md font-serif">Hi there</h4>
+      <h2 class="text-white font-extrabold">{{name}}</h2>
+      <h4 class="text-white sm:text-xs lg:text-md font-serif">{{last_message }}</h4>
     </div>
         
     </div>
 </template>
+<script>
+export default{
+    return : {
+        data : {
+            user : []
+        }
+    }
+}
+</script>
 

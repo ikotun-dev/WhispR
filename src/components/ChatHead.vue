0f817ddf23fb047328fd5f995f7de527a22a3846
Ikotun Collins <danlogan2003@gmail.com>
2023-06-06 18:57:31 +0100

    changes

--- src/components/ChatHead.vue
+++ src/components/ChatHead.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="py-2 px-4 bg-blue-950 h-20 flex justify-between ">
-        <img class="h-16 w-16 rounded-full" :src="require('../assets/images/832.jpg')" >
-        <div class="mr-32">
-         <h2 class="text-white">Collins</h2>
-         <h4 class="text-white font-serif">Hi there</h4>
-        </div>
+    <div class="py-2 px-4 bg-blue-950 h-20 flex">
+        <img class="h-16 w-16 rounded-full " :src="require('../assets/images/832.jpg')" >
+        <div class="ml-4">
+      <h2 class="text-white font-extrabold">Collins</h2>
+      <h4 class="text-white sm:text-xs lg:text-md font-serif">Hi there</h4>
+    </div>
         
     </div>
 </template>
+
--- src/components/ChatTop.vue
+++ src/components/ChatTop.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="bg-blue-900 h-24 pt-4 sm:flex sm:items-center flex-col sm:justify-center sm:flex-col ">
         <img class="sm:h-12 sm:w-12 lg:h-13 lg:w-13 rounded-full" :src="require('../assets/images/832.jpg')" >
-        <h2 class="mt-2 font-extrabold">Collins</h2>
+        <h2 class="sm:mt-2 lg:mt-1 lg:mb-1 font-extrabold">Collins</h2>
     </div>
 </template>
+<style scoped>
+    .wave-emoji {
+        font-family: "Segoe UI Emoji", "NotoColorEmoji", "Twemoji";
+    }
+    
+    
+</style>
